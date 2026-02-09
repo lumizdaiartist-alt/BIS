3fd1f35f28dc12683c48a2c21506bf91c1f9d9b2
lumizdaiartist-alt <lumizdaiartist@gmail.com>
2026-02-09 16:00:53 +0100

    Add alert to indicate dashboard JS loading

--- Admin/dashboard.js
+++ Admin/dashboard.js
@@ -1,3 +1,4 @@
+alert("dashboard js loaded");
 // 🔥 Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
